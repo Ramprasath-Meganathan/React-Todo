6f4a97affa61ba8d4b1e8e69870b1344b2134f6e
Ramprasathmeganathan <66922673+Ramprasath-Meganathan@users.noreply.github.com>
2024-11-14 23:47:11 -0500

    search func initialization

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,26 +3,33 @@ import "./styles.css"
 import { useEffect, useState } from "react"
 import { TodoList } from "./TodoList"
 export default function App() {
-  const [todos, setTodos] = useState(()=>
-  {
+  const [todos, setTodos] = useState(() => {
     const localValue = localStorage.getItem("Items")
-    if(localValue == null) return []
+    if (localValue == null) return []
     return JSON.parse(localValue)
   })
 
-  useEffect(()=>{
-    localStorage.setItem("Items", JSON.stringify(todos))
-  },[todos])
+  const [searchText, setSearchText] = useState("")
+  const handleInputChange = (e) => {
+    setSearchText(e.target.value);
+    todos.map()
+  }
 
-   function addTodo(title)
-    {
-        setTodos(currentTodos => {
-          return [
-            ...currentTodos, { id: crypto.randomUUID(), title, completed: false },
-          ]
-        })
-    }
-  
+  useEffect(() => {
+    localStorage.setItem("Items", JSON.stringify(todos))
+  }, [todos])
+
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos, { id: crypto.randomUUID(), title, completed: false },
+      ]
+    })
+  }
+
+  const todosDataFiltered = todos.filter((todo) =>
+    todos.includes(searchText))
+
   function toggleTodo(id, completed) {
     setTodos(currentTodos => {
       return currentTodos.map(todo => {
@@ -37,16 +44,17 @@ export default function App() {
 
   function deleteTodo(id) {
     setTodos(currentTodos => {
-        return currentTodos.filter(todo => todo.id !== id)
-      }
+      return currentTodos.filter(todo => todo.id !== id)
+    }
     )
   }
 
   return (
     <>
-    <NewTodoForm onSubmit={addTodo}/>
-    <h1 className="header">Todo List</h1>
-    <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <input type="text" id="item" placeholder="Search here.." onChange={handleInputChange} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </>
   )
 }
